4657a0cff161e1a49807f7e0034fd00d45eaff9c
Michal Marciniak <michalmarciniak98@gmail.com>
2026-05-29 12:52:40 +0200

    fix(login-page): correct expected subtitle text for user login page test

--- tests/main.smoke.spec.ts
+++ tests/main.smoke.spec.ts
@@ -18,7 +18,7 @@ test(
 );
 
 test('should load login page', { tag: ['@auth', '@smoke', '@login'] }, async ({ page }) => {
-  const expectedSubtitle = 'User Login & Account Access xddd';
+  const expectedSubtitle = 'User Login & Account Access';
   const loginPage = new LoginPage(page);
 
   await loginPage.goto();
