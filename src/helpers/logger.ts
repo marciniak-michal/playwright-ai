8441a60c3a7838f075c037962cc83ddc62515578
Michal Marciniak <michalmarciniak98@gmail.com>
2026-06-05 17:26:51 +0200

    refactor: restructure logger configuration for improved readability

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,17 +1,17 @@
 import pino from 'pino';
+import PinoPretty from 'pino-pretty';
 
-const logger = pino({
-  name: 'heal',
-  level: process.env.LOG_LEVEL ?? 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:HH:MM:ss',
-      ignore: 'pid,hostname',
-      sync: true,
-    },
+const logger = pino(
+  {
+    name: 'heal',
+    level: process.env.LOG_LEVEL ?? 'debug',
   },
-});
+  PinoPretty({
+    colorize: true,
+    translateTime: 'SYS:HH:MM:ss',
+    ignore: 'pid,hostname',
+    sync: true,
+  })
+);
 
 export default logger;
