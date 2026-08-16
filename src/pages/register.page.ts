30b992071cae103ea049b0b6452b207bd5b7c5a7
Michal Marciniak <michalmarciniak98@gmail.com>
2026-08-16 19:21:58 +0200

    fix: update success message and error alert locators in registration page

--- src/pages/register.page.ts
+++ src/pages/register.page.ts
@@ -26,8 +26,8 @@ export class RegisterPage extends BasePage {
     this.displayNameInput = page.locator('#displayedName');
     this.passwordInput = page.getByTestId('password-input');
     this.submitButton = page.getByTestId('register-submit-btn');
-    this.successMessage = page.getByText('Registration successful!');
-    this.errorAlert = page.locator('[role="alert"]');
+    this.successMessage = page.locator('div.success[role="alert"] div.notification-message');
+    this.errorAlert = page.locator('div.form__error[role="alert"]');
     this.subtitle = page.getByTestId('register-subtitle');
     this.loginHereLink = page.getByTestId('login-link');
     this.backToHomeLink = page.locator('div.auth-form div.auth-links p a[data-testid="home-link"]');
